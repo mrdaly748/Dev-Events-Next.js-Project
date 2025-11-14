d9b6c0bc8f01ab3962d50a58adb10d05bc575099
Hamed <hameddali48@gmail.com>
2025-11-14 13:37:24 +0100

    loading real events

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,45 @@
-import EventCard from "@/components/EventCard"
-import ExploreBtn from "@/components/Explorebtn"
-import { IEvent } from "@/database"
-import { cacheLife } from "next/cache";
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/Explorebtn";
+import { IEvent } from "@/database";
+
 const page = async () => {
   'use cache';
-  cacheLife('hours')
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`, {cache: 'no-store'})
-  const {events} = await response.json()
+
+  // Fetch real events at runtime
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`, {
+    cache: 'no-store', 
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch events');
+  }
+
+  const { events }: { events: IEvent[] } = await response.json();
+
   return (
     <section>
-      <h1 className="text-center">The Hub for Every Dev <br/> Event You Can Not Miss</h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can Not Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
 
       <ExploreBtn />
-      
+
       <div className="mt-20 space-y-7">
-        <h3>Featured Events </h3>
+        <h3>Featured Events</h3>
 
         <ul className="events">
-          {events && events.length > 0 && events.map((event:IEvent)=>(
-            <li key={event.title} className="list-none" >
+          {events.map((event) => (
+            <li key={event.title} className="list-none">
               <EventCard {...event} />
-              
             </li>
           ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
